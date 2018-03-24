30196bf85e489917186ecdf4d958cf4e5ae7eb1a
serp-ya <sergei.kundryukov1989@gmail.com>
2018-03-24 15:42:03 +0300

    Скрипт получения ссылки на регистрацию токена (и полученный токен)

---
+++ get_token.py
@@ -0,0 +1,15 @@
+def make_get_token_url(client_id):
+    get_token_url = 'https://oauth.yandex.ru/authorize'
+    get_token_params = f'response_type=token&client_id={client_id}'
+
+    return f'{get_token_url}?{get_token_params}'
+
+
+def core(client_id):
+    get_token_url = make_get_token_url(client_id)
+    print(get_token_url)
+
+
+if __name__ == '__main__':
+    core('9d452a0a0ec64c7c8f16c08bd83ea097')
+    # { TOKEN: AQAAAAANuCzYAATopD4eBv638Ekdo2RX5DI-hXI }
